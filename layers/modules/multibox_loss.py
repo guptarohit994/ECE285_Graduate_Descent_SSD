62733f4a887b2be65032b9194bc4a84b48394f42
Raghav Subramanian <raghavsubramanian@wireless-169-228-119-225.ucsd.edu>
2019-06-08 19:18:53 -0700

    Updated functions

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from data import coco as cfg
+from data import voc as cfg
 from ..box_utils import match, log_sum_exp
 
 if torch.cuda.is_available(): torch.set_default_tensor_type('torch.cuda.FloatTensor')
